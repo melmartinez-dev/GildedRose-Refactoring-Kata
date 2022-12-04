a600041cd1f04c14b35d2fa8c0e67cc7cdfd452e
mel <melmartinez.dev@gmail.com>
2022-12-04 17:34:29 +0400

    Isolated each type of behavior in private functions

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,14 +1,13 @@
 package com.gildedrose;
 
 class GildedRose {
-    static String AGED_BRIE_NAME = "Aged Brie";
-    static String BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
-    static String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
-    static int ITEM_QUALITY_LOWER_BOUND = 0;
-    static int ITEM_QUALITY_UPPER_BOUND = 50;
-
-    static int BACKSTAGE_PASSES_DOUBLE_QUALITY_MAX_THRESHOLD = 11;
-    static int BACKSTAGE_PASSES_TRIPLE_QUALITY_MAX_THRESHOLD = 6;
+    final static String AGED_BRIE_NAME = "Aged Brie";
+    final static String BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    final static String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+    final static int ITEM_QUALITY_LOWER_BOUND = 0;
+    final static int ITEM_QUALITY_UPPER_BOUND = 50;
+    final static int BACKSTAGE_PASSES_DOUBLE_QUALITY_MAX_THRESHOLD = 11;
+    final static int BACKSTAGE_PASSES_TRIPLE_QUALITY_MAX_THRESHOLD = 6;
 
 
     private Item[] items;
@@ -28,55 +27,39 @@ class GildedRose {
         return items;
     }
 
+    private void updateQualityAgedBrie(Item item) {
+        if (item.quality < ITEM_QUALITY_UPPER_BOUND) item.quality++;
+        item.sellIn--;
+    }
+
+    private void updateQualitySimpleItem(Item item) {
+        if (item.quality > ITEM_QUALITY_LOWER_BOUND) item.quality--;
+        item.sellIn--;
+    }
+
+    private void updateQualityBackstagePasses(Item item) {
+        if (item.quality < ITEM_QUALITY_UPPER_BOUND) item.quality++;
+        if (item.quality < ITEM_QUALITY_UPPER_BOUND && item.sellIn < BACKSTAGE_PASSES_DOUBLE_QUALITY_MAX_THRESHOLD)
+            item.quality++;
+        if (item.quality < ITEM_QUALITY_UPPER_BOUND && item.sellIn < BACKSTAGE_PASSES_TRIPLE_QUALITY_MAX_THRESHOLD)
+            item.quality++;
+        if (item.sellIn == 0) item.quality = ITEM_QUALITY_LOWER_BOUND;
+        item.sellIn--;
+    }
+
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE_NAME)
-                && !item.name.equals(BACKSTAGE_PASSES_NAME)) {
-                if (item.quality > ITEM_QUALITY_LOWER_BOUND) {
-                    if (!item.name.equals(SULFURAS_NAME)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < ITEM_QUALITY_UPPER_BOUND) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES_NAME)) {
-                        if (item.sellIn < BACKSTAGE_PASSES_DOUBLE_QUALITY_MAX_THRESHOLD) {
-                            if (item.quality < ITEM_QUALITY_UPPER_BOUND) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < BACKSTAGE_PASSES_TRIPLE_QUALITY_MAX_THRESHOLD) {
-                            if (item.quality < ITEM_QUALITY_UPPER_BOUND) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals(SULFURAS_NAME)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE_NAME)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES_NAME)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_NAME)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = ITEM_QUALITY_LOWER_BOUND;
-                    }
-                } else {
-                    if (item.quality < ITEM_QUALITY_UPPER_BOUND) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+            switch (item.name) {
+                case AGED_BRIE_NAME:
+                    updateQualityAgedBrie(item);
+                    break;
+                case BACKSTAGE_PASSES_NAME:
+                    updateQualityBackstagePasses(item);
+                    break;
+                case SULFURAS_NAME:
+                    break;
+                default:
+                    updateQualitySimpleItem(item);
             }
         }
     }
